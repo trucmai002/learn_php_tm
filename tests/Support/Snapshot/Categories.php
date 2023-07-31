e4c913d63d0fb0c46c70b17e7700c09120dc92d5
trucmai002 <133090682+trucmai002@users.noreply.github.com>
2023-07-31 13:49:48 +0700

    Create Categories.php

---
+++ tests/Support/Snapshot/Categories.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Support\Snapshot;
+
+class Categories extends \Codeception\Snapshot
+{
+
+
+
+    protected function fetchData()
+    {
+        // TODO: return a value which will be used for snapshot 
+    }
+}
